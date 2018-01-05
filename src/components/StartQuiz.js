b3ecb24a7bf3c68b35336f2beaeb4e03396a0e0b
swaption2009 <swaption2009@gmail.com>
2018-01-04 21:27:45 -0800

    Add Restart, GoBack, and Score functionality to StartQuiz component
    1) create Restart and GoBack functions
    2) create “quiz” state and refresh it when Restart function is invoked
    3) navigate GoBack button to DeckDetail component
    4) show and update Score when user click “correct” or “incorrect”
    buttons

--- src/components/StartQuiz.js
+++ src/components/StartQuiz.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, Button } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Button,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -47,60 +52,128 @@ const styles = {
 
 class StartQuiz extends Component {
   state = {
+    quiz: this.props.quiz,
+    score: 0,
     index: 0,
     showAnswer: false,
+    restartButton: false,
   };
-  onClickedButton = () => {
+
+  onClickedButton = (answer) => {
     this.setState({
       index: this.state.index + 1,
     });
+    if (answer === 'correct') {
+      this.setState({
+        score: this.state.score + 1,
+      });
+    } else {
+      this.setState({
+        score: this.state.score - 1,
+      });
+    }
   };
+
   pickQuestion = (questions, index, length) => {
     if (index <= length - 1) {
       return questions[index];
     } else {
-      this.props.navigation.navigate('Home');
+      this.setState({
+        restartButton: true,
+        index: 0,
+      });
       return questions[0];
     }
   };
+
   showAnswer = () => {
     this.setState({
       showAnswer: true,
     });
   };
+
+  hideAnswer = () => {
+    this.setState({
+      showAnswer: false,
+    });
+  };
+
+  onRestartButton = () => {
+    this.setState({
+      score: 0,
+      quiz: this.props.quiz,
+      restartButton: false,
+    });
+  };
+
+  onGoHomeButton = () => {
+    this.props.navigation.goBack(null);
+  };
+
   render() {
-    const { title, questions } = this.props.quiz;
+    const { title, questions } = this.state.quiz;
+    const { index, score } = this.state;
     const maxIndex = questions.length;
-    const shownQuestion = this.pickQuestion(questions, this.state.index, maxIndex);
+    const shownQuestion = this.pickQuestion(questions, index, maxIndex);
 
     return (
       <View>
         <Text style={styles.textStyle}>
           Topic: {title}
         </Text>
-        <Text style={styles.questionStyle}>
-          Question #{this.state.index + 1}: {shownQuestion.question}
+        <Text style={styles.textStyle}>
+          Score: {score}
         </Text>
-        <TouchableOpacity onPress={() => this.showAnswer()}>
-          <Text style={styles.answerStyle}>Show Answer</Text>
-        </TouchableOpacity>
-        { this.state.showAnswer &&
-        <Text style={styles.questionStyle}>
-          Answer: {shownQuestion.answer}
-        </Text>
+        {!this.state.restartButton &&
+        <View>
+          <Text style={styles.questionStyle}>
+            Question #{index + 1}: {shownQuestion.question}
+          </Text>
+          <TouchableOpacity onPress={() => this.showAnswer()}>
+            <Text style={styles.answerStyle}>Show Answer</Text>
+          </TouchableOpacity>
+
+          { this.state.showAnswer &&
+          <View>
+            <Text style={styles.questionStyle}>
+              Answer: {shownQuestion.answer}
+            </Text>
+            <TouchableOpacity onPress={() => this.hideAnswer()}>
+              <Text style={styles.answerStyle}>Hide Answer</Text>
+            </TouchableOpacity>
+          </View>
+          }
+
+          <Button
+            onPress={() => this.onClickedButton('correct')}
+            title="Correct"
+            style={styles.buttonStyle}
+            color="#1565C0"
+          />
+          <Button
+            onPress={() => this.onClickedButton('incorrect')}
+            title="Incorrect"
+            style={styles.buttonStyle}
+            color="#AD1457"
+          />
+        </View>
         }
-        <Button
-          onPress={() => this.onClickedButton()}
-          title="Correct"
-          style={styles.buttonStyle}
-          color="#1565C0"
-        />
-        <Button
-          onPress={() => this.onClickedButton()}
-          title="Incorrect"
-          style={styles.buttonStyle}
-          color="#AD1457"
-        />
+        { this.state.restartButton &&
+        <View>
+          <Button
+            onPress={() => this.onRestartButton('correct')}
+            title="Restart Quiz"
+            style={styles.buttonStyle}
+            color="#1565C0"
+          />
+          <Button
+            onPress={() => this.onGoHomeButton()}
+            title="Go Back to Deck"
+            style={styles.buttonStyle}
+            color="#AD1457"
+          />
+        </View>
+        }
       </View>
     );
   }
